6e07fa82d78dbf633d85b0a7ac6b47447228ee2b
mowslasher83 <radeswaled@hotmail.com>
2025-09-03 12:11:33 -0500

    feat: initial commit for components/Cards.js

---
+++ components/Cards.js
@@ -0,0 +1,14 @@
+// Cards component
+
+import React from 'react';
+
+const Card = ({ title, content }) => {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>{content}</p>
+    </div>
+  );
+};
+
+export default Card;
