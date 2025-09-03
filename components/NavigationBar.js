6438fae639c291192e0f10a0c0aedd129d9ce565
mowslasher83 <radeswaled@hotmail.com>
2025-09-03 12:11:34 -0500

    feat: initial commit for components/NavigationBar.js

---
+++ components/NavigationBar.js
@@ -0,0 +1,17 @@
+// Navigation Bar component
+
+import React from 'react';
+
+const NavigationBar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Trang chủ</li>
+        <li>Giới thiệu</li>
+        <li>Liên hệ</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
