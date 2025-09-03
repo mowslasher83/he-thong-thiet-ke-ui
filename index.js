fc65f1840eca010dbd2cb17aa2552d7d2fc92fd6
mowslasher83 <radeswaled@hotmail.com>
2025-09-03 12:16:53 -0500

    Refactor console log messages for consistency

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Entry point for the UI design system
 
-console.log('UI design system is ready!');
+console.log('UI Design System is ready!');
 console.log('Welcome to the UI Design System! Enjoy exploring the features.');
