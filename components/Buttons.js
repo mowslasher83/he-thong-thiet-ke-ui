65972523fb549071e35e246b7f85dea0dbee7890
mowslasher83 <radeswaled@hotmail.com>
2025-09-03 12:11:29 -0500

    feat: initial commit for components/Buttons.js

---
+++ components/Buttons.js
@@ -0,0 +1,13 @@
+// Buttons component
+
+import React from 'react';
+
+const Button = ({ label, onClick }) => {
+  return (
+    <button onClick={onClick} className="btn">
+      {label}
+    </button>
+  );
+};
+
+export default Button;
